a8548bf5c13ea016bd39882f3142b809d30ba740
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-05-02 15:11:21 +0200

    inseriti i vari commenti

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -6,12 +6,20 @@
 
 // creare elenco di numeri da 1 a 100
 
+// dare una "variabile" di nome contenitore e selezionarlo dal documento (prendere quindi classe contenitore all'interno del div)
 const contenitore = document.querySelector("div.contenitore");
 
+
+
+
+// creare un ciclo che fa da numero 1 a 100 compreso
 for (let i = 1; i <= 100; i++) {
 
 
+    // creare "variabile" di nome contenuto che stamperà successivamente il testo corrispettivo, inserendolo direttamente dentro il tag div (con uno span)
     const contenuto = `<span>${i}</span>`
+    
+    // inserire all'interno del contenitore il contenuto (+ e = per far entrare tutti i numeri)
     contenitore.innerHTML += contenuto;
     
 
